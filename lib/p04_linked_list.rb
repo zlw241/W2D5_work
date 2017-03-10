17a9aa52d302b39859b445eb3ff03769ca60e6f7
Zach Ward <zach.l.ward6@gmail.com>
2017-03-10 15:44:35 -0500

    Implement linked list

--- lib/p04_linked_list.rb
+++ lib/p04_linked_list.rb
@@ -1,3 +1,5 @@
+require 'byebug'
+
 class Link
   attr_accessor :key, :val, :next, :prev
 
@@ -19,7 +21,13 @@ class Link
 end
 
 class LinkedList
+  include Enumerable
+  attr_accessor :head, :tail, :current
+
   def initialize
+    @head = Link.new
+    @tail = Link.new
+    @current = head
   end
 
   def [](i)
@@ -28,30 +36,81 @@ class LinkedList
   end
 
   def first
+    head.next
   end
 
   def last
+    tail.prev
   end
 
   def empty?
+    current == head
   end
 
   def get(key)
+    self.each do |link|
+      return link.val  if link.key == key
+    end
   end
 
   def include?(key)
+    each do |link|
+      return true if link.key == key
+    end
+    false
   end
 
   def append(key, val)
+    # debugger
+    new_link = Link.new(key, val)
+    new_link.prev = current
+    current.next = new_link
+    new_link.next = tail
+    self.current = new_link
+    tail.prev = current
+    # debugger
   end
 
   def update(key, val)
+    return "empty" if empty?
+    # debugger
+    self.each do |link|
+      # debugger
+      if link.key == key
+        link.val = val
+      end
+
+    end
+    # debugger
   end
 
   def remove(key)
+    self.each do |link|
+      if link.key == key
+        link.prev.next = link.next
+        link.next.prev = link.prev
+      end
+    end
   end
 
-  def each
+  def each(&prc)
+    idx = head
+    # while idx.next != nil
+    #   # prc.call(p idk)
+    #   prc.call(idx)
+    #   idx = idx.next
+    #
+    # end
+
+    until idx == last
+      idx = idx.next
+      prc.call(idx)
+      # idx = idx.next
+
+
+    end
+    # prc.call(idx)
+
   end
 
   # uncomment when you have `each` working and `Enumerable` included
